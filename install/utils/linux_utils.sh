96ef9db2f635ec441529ae993a7695f77e8542be
BuildTools <unconfigured@null.spigotmc.org>
2024-04-15 01:04:48 -0400

    Removed install folder

--- install/utils/linux_utils.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Add Linux-specific installation commands here
-echo "Running Linux-specific installation steps..."
-# Example: Install dependencies using the package manager (apt-get, yum, etc.)
-
-#echo "[Linux Utils] Installing server"
--- install/utils/mac_utils.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Add MacOS-specific installation commands here
-echo "Running MacOS-specific installation steps..."
-# Example: Install dependencies using Homebrew or MacPorts
