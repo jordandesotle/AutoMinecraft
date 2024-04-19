756727e937724763b0b4940a7a93dabdb815c53d
BuildTools <unconfigured@null.spigotmc.org>
2024-04-19 06:01:24 -0400

    Added hyperparameters to model_architecture file

--- neural_network/models/model_architecture.py
+++ neural_network/models/model_architecture.py
@@ -2,30 +2,32 @@
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
 from config.config import Config
+from config.hyperparameters import Hyperparameters
 
 def build_model(input_shape=Config.INPUT_SHAPE, num_classes=Config.NUM_CLASSES):
     model = Sequential()
 
     # Convolutional layers
-    model.add(Conv2D(32, (3, 3), activation='relu', input_shape=input_shape))
+    model.add(Conv2D(Hyperparameters.CONV1_FILTERS, (3, 3), activation='relu', input_shape=input_shape))
     model.add(MaxPooling2D((2, 2)))
 
-    model.add(Conv2D(64, (3, 3), activation='relu'))
+    model.add(Conv2D(Hyperparameters.CONV2_FILTERS, (3, 3), activation='relu'))
     model.add(MaxPooling2D((2, 2)))
 
-    model.add(Conv2D(128, (3, 3), activation='relu'))
+    model.add(Conv2D(Hyperparameters.CONV3_FILTERS, (3, 3), activation='relu'))
     model.add(MaxPooling2D((2, 2)))
 
     # Flatten layer to transition from convolutional to fully connected layers
     model.add(Flatten())
 
     # Fully connected layers
-    model.add(Dense(128, activation='relu'))
+    model.add(Dense(Hyperparameters.DENSE_UNITS, activation='relu'))
     model.add(Dense(num_classes, activation='softmax'))
 
     return model
 
 if __name__ == "__main__":
+    
     # Print a summary of the model architecture
     model = build_model()
     model.summary()
